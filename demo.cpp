026769d6cf21626eacd3701fc6d8a121d8b30c4d
Waris Shaik <shaikwaris61@gmail.com>
2023-09-16 09:45:39 +0530

    updated

--- demo.cpp
+++ demo.cpp
@@ -1,25 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int getMax(int arr[], int size){
-    int max = arr[0];
-    for(int i=1; i<size; i++){
-        if(arr[i] > max){
-            max = arr[i];
-        }
+void reverseArray(int arr[], int size){
+    int start = 0;
+    int end = size - 1;
+    while(start <= end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-    return max;
-}
 
-
-int getMin(int arr[], int size){
-    int min = arr[0];
-    for(int i=1; i<size; i++){
-        if(arr[i] < min){
-            min = arr[i];
-        }
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
     }
-    return min;
+    
 }
 
 
@@ -27,9 +23,8 @@ int getMin(int arr[], int size){
 
 int main(){
 
-int arr[] = {1,5,8,9,65,7};
-cout << getMax(arr, 6) << endl;
-cout << getMin(arr,6);
+int arr[6] = {1,5,8,9,65,7};
+reverseArray(arr, 6);
 
   
     
--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// another approach
+void reverseArray(char arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        char temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
 int main()
 {
-    char arr[5] = {'w', 'a', 'r', 'i', 's'}; 
+    char arr[5] = {'w', 'a', 'r', 'i', 's'};
     int length = sizeof(arr) / sizeof(char);
 
-    for (int i = 0; i < length / 2; i++)
+    for (int i = 0; i < length / 2; i++) // one approach
     {
         char temp = arr[i];
         arr[i] = arr[length - 1 - i];
@@ -17,4 +38,6 @@ int main()
     {
         cout << arr[i] << " ";
     }
+
+    // reverseArray(arr, 5);
 }
