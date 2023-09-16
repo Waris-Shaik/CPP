f8a82e1c4ed83d05ca04c740be48b49d04f421b2
Waris Shaik <shaikwaris61@gmail.com>
2023-09-16 13:48:35 +0530

    initial commit

--- demo.cpp
+++ demo.cpp
@@ -1,25 +1,5 @@
 #include<iostream>
 using namespace std;
-
-void swapAlternative(int arr[], int size){
-    int start = 0;
-    int end = size +1;
-
-    while(start <= end){
-        int temp = arr[start];
-        arr[start] =  arr[start+1];
-        arr[start+1] = temp;
-        start+=2;
-        end--;
-    }
-   
-
-    for(int i=0; i<size; i++){
-        cout << arr[i] << " ";
-    }
-}
-
 int main(){
-    int arr[8] = {1,5, 6,9, 11,32, 45,85};
-    swapAlternative(arr,8);
+    cout << "Hello cpp";
 }
---
+++ index.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+
+int main(){
+
+    int arr[7] = {2,3,1,6,3,6,2};
+    int ans = 0;
+
+    for(int i=0; i<7; i++){
+        ans = ans ^ arr[i];
+    }
+
+
+cout << ans << endl;
+
+    
+}
